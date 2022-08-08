88f6cc19892b774b4d3b8c9b8d96d260344fbb51
shraddhabaheti <shraddhabaheti123@gmail.com>
2022-08-08 18:27:02 +0530

    comments

--- src/Getpostdata.js
+++ src/Getpostdata.js
@@ -7,13 +7,30 @@ import ThumbDownIcon from '@mui/icons-material/ThumbDown';
 import ThumbDownOffAltIcon from '@mui/icons-material/ThumbDownOffAlt';
 import './App.css';
 import axios from "axios";
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 function Getpostdata() {
     const [postData, setPostData] = useState([])
-    const [likes, setlike] = useState(false)
-   const [btnColor, setBtnColor] = useState("red");
-    let _id=localStorage.getItem('_id')
+    const [likes, setlike] = useState(true)
+    const [btnColor, setBtnColor] = useState("red");
     
+    const [state, setState] = useState({
+        text: '',
+
+    })
+    let _id = localStorage.getItem('_id')
+    const handleChange = (e) => {
+        setState({
+            ...state,
+            [e.target.name]: e.target.value
+        })
+
+    }
+
+    console.log('state', state)
+
     const getData = async () => {
         const token = localStorage.getItem("token")
         let response = await fetch('http://192.168.1.27:4000/posts/getallposts', {
@@ -24,11 +41,11 @@ function Getpostdata() {
         });
         let user = await response.json();
         setPostData(user)
-         
-      }
+
+    }
     useEffect(() => {
         getData();
-       //setlike(_id)
+
     }, [])
     console.log(postData)
     const like = async (data) => {
@@ -40,21 +57,18 @@ function Getpostdata() {
             const token = localStorage.getItem('token')
             const res = await axios.put(`http://192.168.1.27:4000/posts/likes`, { postid: data._id }, {
                 headers: {
-                    'Authorization': 'Bearer ' + token
+                    'Authorization': `Bearer ${token}`
                 }
             })
-          
+
             setlike(res?.data)
             setlike(token)
-           getData()
-               
-
-
+            getData()
         } catch (error) {
             console.log(error)
         }
 
-       
+
     }
     const dislike = async (data) => {
         try {
@@ -62,11 +76,12 @@ function Getpostdata() {
             const token = localStorage.getItem('token')
             let responce = await axios.put(`http://192.168.1.27:4000/posts/unlikes`, { postid: data._id }, {
                 headers: {
-                    'Authorization': 'Bearer ' + token
+
+                    'Authorization': `Bearer ${token}`
                 }
 
             })
-            
+
             setlike(responce?.data)
             setlike(token)
             getData()
@@ -76,7 +91,7 @@ function Getpostdata() {
             console.log(error)
 
         }
-        
+
     }
     const deletes = async (data) => {
 
@@ -96,16 +111,34 @@ function Getpostdata() {
         }
 
     }
-   
 
+    const comment = async (data) => {
+
+        try {
+            const token = localStorage.getItem('token')
+            let responce = await axios.put(`http://192.168.1.27:4000/posts/comments`,
+                { postid: data._id, text: state.text },
+                {
+                    headers: {
+                        'Authorization': `Bearer ${token}`,
+                    },
+                })
+
+
+        } catch (error) {
+            console.log(error)
+
+        }
+    }
+    // debugger
     return (
         <div>
             <Color />
             <h1 className="h1">Get post data</h1>
             {
-            
+
                 postData.map((value, i) => {
-                
+
                     return (
 
                         <div id={value._id}>
@@ -113,16 +146,29 @@ function Getpostdata() {
                             <h5>{value.content}</h5>
                             <img className="image1" src={value.image[0].replace('localhost', '192.168.1.27')} />
                             <br />
-                            { 
+                            {
 
                                 value.likes.includes(_id) ?
-                                     <ThumbDownOffAltIcon onClick={() => dislike(value)}></ThumbDownOffAltIcon>
-                                    : <ThumbUpIcon style={{ color: btnColor ? 'blue' : '' }} onClick={()=>like(value)}></ThumbUpIcon>
+                                    <FavoriteBorderIcon onClick={() => dislike(value)}></FavoriteBorderIcon>
+                                    : <FavoriteIcon style={{ color: btnColor ? 'blue' : '' }} onClick={() => like(value)}></FavoriteIcon>
                             }
-
                             <h6>{value?.likes.length} likes </h6>
                             <DeleteIcon variant="danger" onClick={() => deletes(value)}>Delete</DeleteIcon>
-                            {/* <button>Edit</button> */}
+                            <div>
+                                {
+                                    value.comments?.map((value, i) => {
+                                        return (
+                                            <div className="comments">
+                                                <h6>{value.postedBy.name}</h6>
+                                                <p>{value.text}</p>
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </div>
+                            <input type="text" name="text" onChange={handleChange} />
+
+                            <button onClick={() => comment(value)}>comment</button>
                         </div>
 
                     )
--- src/PostData.js
+++ src/PostData.js
@@ -56,6 +56,7 @@ function PostData() {
 
         }
      }
+     
     return (
         <div>
             <Color />
